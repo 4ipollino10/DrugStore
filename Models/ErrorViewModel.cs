d020f7aead5e0885133f14617ef425899d79e7f0
Timur <t.gulyaev@g.nsu.ru>
2023-03-03 14:57:37 +0700

    initialize

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrugStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
